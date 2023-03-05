06ea1ce4ca05ccd15e1f48f8f6f1c6a07875719d
SarokaAnton <tonysorokaol@gmail.com>
2023-03-05 12:15:50 +0100

    added form in panel

--- Lab_01/PluginForAds/PluginForAds.php
+++ Lab_01/PluginForAds/PluginForAds.php
@@ -40,7 +40,96 @@ function option_admin_page() {
 
 function menu_plugin_for_ads_cb(){
 
-    echo "Hell world";
+    global $_POST;
+
+	// if(isset($_POST['form_do_change'])){
+	// 	if($_POST['form_do_change'] == 'Y'){
+	// 		$formName = $_POST['name'];
+	// 		$formContent = $_POST['content'];
+	// 		$formTimeStart = $_POST['time_start'];
+	// 		$formTimeEnd = $_POST['time_end'];
+	// 		$formActiveHoursStart = $_POST['active_hours_start'];
+	// 		$formActiveHoursStop = $_POST['active_hours_start'];
+	// 		if($_POST['active']) $formActive = TRUE;
+	// 		else $formActive = FALSE;
+
+	// 		echo '<div class="notice notice-success isdismissible">
+	// 		<p>Advertisment saved.</p>
+	// 		</div>';
+			
+	// 		update_option('name', $formName);
+	// 		update_option('content', $formContent);
+	// 		update_option('time_start', $formTimeStart);
+	// 		update_option('time_end', $formTimeEnd);
+	// 		update_option('active_hours_start', $formActiveHoursStart);
+	// 		update_option('active_hours_stop', $formActiveHoursStop);
+	// 		update_option('active', $formActive);
+	// 	}
+	// }
+?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="form.css">
+	</head>
+	<body>
+		<div class="container">
+			<h2>Add New Advertisement</h2>
+			<form>
+				<input type="hidden" name="form_do_change" value="Y">
+				<div class="row">
+					<div class="col-25">
+						<label for="name">Name:</label>
+					</div>
+					<div class="col-75">
+						<input type="text" id="name" name="name" placeholder="Name of advertisment">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<label for="content">Content:</label>
+					</div>
+					<div class="col-75">
+						<textarea rows="4" cols="50" id="content" name="content" placeholder="Content of advertisment"></textarea>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<label for="time_start">Start date:</label>
+						<input type="date" id="time_start" name="time_start" min="2023-01-03">
+					</div>
+					<div class="col-75">
+						<label for="time_end">End date:</label>
+						<input type="date" id="time_end" name="time_end" min="2023-01-01">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<label for="active_hours_start">Start time:</label>
+						<input type="time" id="active_hours_start" name="active_hours_start">
+					</div>
+					<div class="col-75">
+						<label for="active_hours_stop">End time:</label>
+						<input type="time" id="active_hours_stop" name="active_hours_stop">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<input type="radio" id="active" name="active" value="Active" checked>
+						<label for="active">Active</label>
+						<input type="radio" id="inactive" name="active" value="Inactive">
+						<label for="inactive">Inactive</label>
+					</div>
+				</div>
+				<div class="row">
+					<input type="submit" value="Add">
+				</div>
+			</form>
+		</div>
+	</body>
+	</html>
+<?php
 }
 
 function jal_install() {
