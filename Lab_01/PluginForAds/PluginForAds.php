030f88cd3a52226b2a3cf90c468a2372af3fbd00
SarokaAnton <tonysorokaol@gmail.com>
2023-03-06 20:13:23 +0100

    add css and back btn

--- Lab_01/PluginForAds/PluginForAds.php
+++ Lab_01/PluginForAds/PluginForAds.php
@@ -28,7 +28,7 @@ function get_random_ad(){
 	$res = '';
     global $wpdb;
     $table_name = $wpdb->prefix . "ad_plugin";
-    $all_adver = $wpdb->get_results( "SELECT * FROM $table_name Where " );
+    $all_adver = $wpdb->get_results( "SELECT * FROM $table_name" );
 	if(count($all_adver)>0){
 		$rand_index = array_rand($all_adver, 1);
 		$rand_ad = $all_adver[$rand_index];
@@ -64,6 +64,7 @@ function add_mod_advertisment_form()
 	$val_start_time = '';
 	$val_end_time = '';
 	$val_is_active = '';
+	$title_type ='Add New Advertisement'
 	$form_type = '<input type="hidden" name="action" value="add_mod_advertisment" />
 	<input type="hidden" name="action_type" value="add" />
 	<input type="submit" name="add_new_advertisment" value="Add">';
@@ -79,6 +80,7 @@ function add_mod_advertisment_form()
 		$val_start_time = $advert->active_hours_start;
 		$val_end_time = $advert->active_hours_stop;
 		$val_is_active = $advert->active;
+		$title_type ='Edit Advertisment'
 
 		$form_type = '<input type="hidden" name="action" value="add_mod_advertisment" />
 	<input type="hidden" name="action_type" value="mod" />
@@ -90,8 +92,15 @@ function add_mod_advertisment_form()
 
 	$res = '';
 
-	$res = '<div class="container">
-		<h2>Add New Advertisement</h2>
+	$res = '
+	<div class="container">
+		<div class="row">
+			<form method="post">
+				<input type="hidden" name="back" value="back_to_main"/>
+				<input type="submit" class="back-button" name="back_btn" value="&laquo;  Back"/>
+			</form>
+		</div>
+		<h2>'. $title_type .'</h2>
 		<form method="post">
 			<input type="hidden" name="form_do_change" value="Y">
 			<div class="row">
@@ -99,7 +108,7 @@ function add_mod_advertisment_form()
 					<label for="name">Name:</label>
 				</div>
 				<div class="col-75">
-					<input type="text" id="name" name="name" placeholder="Name of advertisment" value="' . $val_name . '">
+					<input type="text" id="name" name="name" placeholder="Name of advertisment" value="' . $val_name . '" required>
 				</div>
 			</div>
 			<div class="row">
@@ -107,31 +116,46 @@ function add_mod_advertisment_form()
 					<label for="content">Content:</label>
 				</div>
 				<div class="col-75">
-					<textarea rows="4" cols="50" id="content" name="content" placeholder="Content of advertisment">' . $val_content . '</textarea>
+					<textarea rows="4" cols="50" id="content" name="content" placeholder="Content of advertisment" required>' . $val_content . '</textarea>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-25">
 					<label for="time_start">Start date:</label>
-					<input type="date" id="time_start" name="time_start" min="2023-01-03"  value="' . $val_start_date . '">
 				</div>
 				<div class="col-75">
+					<input type="date" id="time_start" name="time_start" min="2023-01-03"  value="' . $val_start_date . '" required>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-25">
 					<label for="time_end">End date:</label>
-					<input type="date" id="time_end" name="time_end" min="2023-01-01"  value="' . $val_end_date . '">
+				</div>
+				<div class="col-75">
+					<input type="date" id="time_end" name="time_end" min="2023-01-01"  value="' . $val_end_date . '" required>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-25">
 					<label for="active_hours_start">Start time:</label>
-					<input type="time" id="active_hours_start" name="active_hours_start"  value="' . $val_start_time . '">
 				</div>
 				<div class="col-75">
-					<label for="active_hours_stop">End time:</label>
-					<input type="time" id="active_hours_stop" name="active_hours_stop"  value="' . $val_end_time . '">
+					<input type="time" id="active_hours_start" name="active_hours_start"  value="' . $val_start_time . '" required>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-25">
+					<label for="active_hours_stop">End time:</label>
+				</div>
+				<div class="col-75">
+					<input type="time" id="active_hours_stop" name="active_hours_stop"  value="' . $val_end_time . '" required>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-25">
+				
+				</div>
+				<div class="col-75">
 					<input type="radio" id="active" name="active" value="Active" checked>
 					<label for="active">Active</label>
 					<input type="radio" id="inactive" name="active" value="Inactive">
@@ -233,25 +257,31 @@ function render_list_of_advertisments()
 	// 	echo $_POST['mod_del_id'];
 	// }
 	echo '
-	<form method="post">
-	<input type="hidden" name="page" value="menu_plugin_for_ads" /> 
-	<input type="submit" name="add" value="Add new advertisment"/>	
+	<div class="container">
+		<div class="row">
+			<form method="post">
+				<input type="hidden" name="page" value="menu_plugin_for_ads" /> 
+				<input type="submit" class="add-new-btn" name="add" value="Add new advertisment"/>	
 			</form>
+		</div>	
+		<div class="row">
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Start Date</th>
+						<th>End Date</th>
+						<th>Active Start Time</th>
+						<th>Active End Time</th>
+						<th>Is Active?</th>
+						<th></th>
+					</tr>
+				</thead>';
 
-<table>
-    <tr>
-        <th>Name</th>
-        <th>Start Date</th>
-        <th>End Date</th>
-        <th>Active Start Time</th>
-        <th>Active End Time</th>
-        <th>Is Active?</th>
-        <th></th>
-    </tr>
-	';
 	foreach ($all_adver as $adver) {
 
-		echo '<tr>
+		echo '
+		<tr>
             <td>' . $adver->name . '</td>
             <td>' . $adver->time_start . '</td>
             <td>' . $adver->time_end . '</td>
@@ -261,13 +291,15 @@ function render_list_of_advertisments()
             <td>
 				<form method="post">
 					<input type="hidden" name="mod_del_id" value="' . $adver->id . '" /> 
-					<input type="submit" name="edit" value="Edit">
-					<input type="submit" name="delete" value="Delete">
+					<input type="submit" class="delete-btn" name="delete" value="Delete"/>
+					<input type="submit" class="edit-btn" name="edit" value="Edit"/>
 				</form>
             </td>
         </tr>';
 	}
-	echo '</table>';
+	echo '
+		</table>
+	</div>';
 }
 
 function delete_advertisment($id)
@@ -302,7 +334,7 @@ function menu_plugin_for_ads_cb()
 
 function ad_register_styles()
 {
-	wp_register_style('form_styles', plugins_url('/css/form.css', __FILE__));
+	wp_register_style('form_styles', plugins_url('/css/style.css', __FILE__));
 	wp_enqueue_style('form_styles');
 }
 
