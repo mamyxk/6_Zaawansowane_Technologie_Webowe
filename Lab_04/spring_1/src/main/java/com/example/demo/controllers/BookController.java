eb19ddb40a6588176c3c7e6dfcbdb03251e70be7
kamil.haniszewski <kamil.haniszewski@systemeg.net>
2023-04-03 22:46:46 +0200

    fix

--- Lab_04/spring_1/src/main/java/com/example/demo/controllers/BookController.java
+++ Lab_04/spring_1/src/main/java/com/example/demo/controllers/BookController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Author;
 import com.example.demo.models.Book;
+import com.example.demo.repository.AuthorRepository;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.request.BookRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,16 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     @PostMapping("/add-book")
     public ResponseEntity<Book> createBook(@RequestBody BookRequest bookRequest) {
-        Author author = new Author();
-        author.setId(bookRequest.getAuthorId());
+        var optionalAuthor = authorRepository.findById(bookRequest.getAuthorId());
+        if (optionalAuthor.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        Author author = optionalAuthor.get();
 
         Book book = new Book();
         book.setName(bookRequest.getName());
--- Lab_04/spring_1/src/main/java/com/example/demo/repository/AuthorRepository.java
+++ Lab_04/spring_1/src/main/java/com/example/demo/repository/AuthorRepository.java
@@ -5,6 +5,11 @@ import com.example.demo.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+//    Optional<Author> findById(Long id){
+//
+//    }
 }
